4011622e10fd37187cf87a46576f60741e5d9ef0
ahad abdul <ahad.a2703@gmail.com>
2022-06-25 01:47:17 +1000

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+  }
+};
