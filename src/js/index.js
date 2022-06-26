9ad068c1c44f5f632b5c1f2ae662e205dc815176
ahad abdul <ahad.a2703@gmail.com>
2022-06-26 20:13:46 +1000

    added weather display

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../css/styles.css';
+import '../css/index.css';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
@@ -13,18 +13,21 @@ async function getWeather(location) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a6dc08ad1a525beab23e4ffda17b9580&units=metric`);
   if (response.status === 200) {
     const data = await response.json();
+
+    localStorage.setItem("data", JSON.stringify(data));
     error.style.visibility = "hidden";
     form.submit();
   }
   else {
     error.style.visibility = "visible";
     form.reset();
-    return response.status;
   }
-}
+};
+
 
 const submitForm = (e) => {
   e.preventDefault();
+  localStorage.clear();
   getWeather(input.value);
 }
 
--- src/js/results.js
+++ src/js/results.js
@@ -1,5 +1,30 @@
+import '../css/results.css';
+
+const weatherData = JSON.parse(localStorage.getItem("data"));
+console.log(weatherData);
 
 
+const weatherDescription = document.querySelector(".weather-desc");
+const weatherLocation = document.querySelector(".weather-location");
+const weatherTemp = document.querySelector(".temp-value");
+const weatherTempUnit = document.querySelector(".temp-unit");
+const feelsLikeInfo = document.querySelector(".feels-like");
+const windInfo = document.querySelector(".wind");
+const humidityInfo = document.querySelector(".humidity");
+let celsius = '&#8451;';
+console.log(weatherData.weather[0].description);
+console.log(weatherData.name);
+console.log(weatherData.sys.country);
+console.log(weatherData.main.temp);
+console.log(weatherData.main.feels_like);
+console.log(weatherData.wind.speed);
+console.log(weatherData.main.humidity);
 
 
-//getWeather(searchResult);
+weatherDescription.innerText = (weatherData.weather[0].description).toUpperCase();
+weatherLocation.innerText = `${weatherData.name}, ${weatherData.sys.country}`;
+weatherTemp.innerText = `${weatherData.main.temp}`;
+weatherTempUnit.innerHTML = `${celsius}`;
+feelsLikeInfo.innerHTML = `<span class = "sub-info">FEELS LIKE: </span>` + weatherData.main.feels_like;
+windInfo.innerHTML = `<span class = "sub-info">WIND: </span>`+ weatherData.wind.speed;
+humidityInfo.innerHTML = `<span class = "sub-info">HUMIDITY: </span>`+ weatherData.main.humidity;
